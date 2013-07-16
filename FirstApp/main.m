1704dfecb0d6164e2d71b51c66fcebdd87eee629
Ameya Nerurkar <ameyanerurkar@yahoo.com>
2013-07-15 23:20:33 -0700

    Initial Commit

---
+++ FirstApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Ameya Nerurkar on 7/15/13.
+//  Copyright (c) 2013 Ameya Nerurkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
